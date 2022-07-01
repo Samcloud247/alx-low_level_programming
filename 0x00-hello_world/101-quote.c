8bf454bf42077726101d1891622dc9f28bcbc57a
samcloud247 <sam.cloud742@gmail.com>
2022-06-30 20:40:54 -0700

    task 8.1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <unistd.h>
+/** 
+* main - Entry Point 
+* Return: 1 (Failure) 
+*/
+int main(void)
+{write
+(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
