920209772f105c3127081d73c00afce1fdb45882
samcloud247 <sam.cloud742@gmail.com>
2022-06-30 21:30:26 -0700

    task 666

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
-ii#include <stdio.h>
+#include <stdio.h>
 /**
 * main - prints the size of various types
 * Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
+{puts
+("\"Programming is like building a multilingual puzzle");
 return (0);
 }
