09cd56b86463009fff733840e59dad3897d892c3
samcloud247 <sam.cloud742@gmail.com>
2022-07-02 17:02:06 -0700

    task 2.2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 /**
 * *main - print all letters of the alphabeth in lower case
-*
 * *Return: 0 if successful
 */
 int main(void)
 {
 	char var;
 
-	for (var = 'a'; var <= 'z'; var++)		    
+	for (var = 'a'; var <= 'z'; var++)
 	{
-		      putchar(var);
+		putchar(var);
 	}
-	putchar('\n');
+		putchar('\n');
 	return (0);
 }
